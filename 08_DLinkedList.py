073a4932b573bdec6408b68268032402e3972cbf
Sdey <shreyandey1234@gmail.com>
2025-07-13 19:57:39 +0530

    add update dll

--- 08_DLinkedList.py
+++ 08_DLinkedList.py
@@ -13,12 +13,32 @@ class DLL:
             print("Linked list is empty !! ")
         else:
             currentNode = self.head
-            print("None > ")
+            print("None > ", end="")
             while currentNode is not None:
                 print(currentNode.data, end=" > ")
                 currentNode = currentNode.next
             print("None")
 
+    def addStart(self, data):
+        if self.head is None:
+            newNode = node(data)
+            self.head = newNode
+        else:
+            newNode = node(data)
+            newNode.next = self.head
+            self.head = newNode
+
+
+
+
+
+
+
 dll1 = DLL()
+
+dll1.addStart(10)
+dll1.addStart(20)
+dll1.addStart(30)
+dll1.addStart(40)
 
 dll1.trav()
