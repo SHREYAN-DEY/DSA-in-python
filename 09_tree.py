9fb6b2a95e4bf88822f16bdfdd24dbf81646512e
SDey <shreyandey1234@gmail.com>
2025-07-16 03:09:52 -0700

    add new info 09_tree

--- 09_tree.py
+++ 09_tree.py
@@ -4,6 +4,33 @@ class bst:
         self.lchild = None
         self.rchile = None
 
-root = bst(10)
+    def insert(self, data):
+        if self.key is None:
+            self.key = data
+            return
+        elif self.key > data:
+            if self.lchild:
+                self.lchild.insert(data)
+            else:
+                self.lchild = bst(data)
+
+root = bst(None)
+root.insert(10)
 
 print(root.key)
+
+# class node:
+#     def __init__(self,key):
+#         self.lchild = None
+#         self.key = key
+#         self.rchild = None
+
+# class bst:
+#     def __init__(self):
+#         self.root = None
+    
+#     def addChild(self, key):
+#         newNode = node(key)
+#         if self.root is None:
+#             self.key = newNode
+#         elif 
