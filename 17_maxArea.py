92b6ec18c742aebd90257c2d4e64b92bb0463732
SHREYAN <shreyandey1234@gmail.com>
2025-09-17 15:16:01 +0530

    add 17_maxArea.py

--- 17_maxArea.py
+++ 17_maxArea.py
@@ -1,19 +1,35 @@
 # BRUTE FORCE
+# def maxArea(height):
+#     res = 0
+#     for l in range(len(height)):
+#         for r in range( l + 1, len(height)):
+#             w = ((r+1)-(l+1))
+#             h = min(height[l], height[r])
+#             area = w * h
+#             res = max(res, area)
+#     return res
+
+
+# OPTIMAL( TWO POINTER )
 def maxArea(height):
     res = 0
-    for l in range(len(height)):
-        for r in range( l + 1, len(height)):
-            w = ((r+1)-(l+1))
-            h = min(height[l], height[r])
-            area = w * h
-            res = max(res, area)
+    l , r = 0, len(height) - 1
+    while l < r:
+        w = ((r+1)-(l+1))
+        h = min(height[l], height[r])
+        area = w * h
+        res = max(res, area)
+        if height[l] < height[r]:
+            l += 1
+        elif height[l] > height[r]:
+            r -= 1
+        else:
+            l += 1
     return res
+
+
+
+
 
 height = [1,7,2,5,4,7,3,6]
 print(maxArea(height))
-
-
-
-
-
-
